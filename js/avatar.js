cbe3d6ad4777f6510691f0e0a2879b8c3898dcc3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-18 20:45:11 -0500

    [+] Avatar: Crea Componente usando React

--- js/avatar.js
+++ js/avatar.js
@@ -1,24 +1,17 @@
-const $app = document .getElementById( 'app' );
+
+const 
+    $app = document .getElementById( 'app' ),
+    el = React .createElement;
 
 const Avatar = params => {
     const 
         src = `https://randomuser.me/api/portraits/women/${ params .id }.jpg`,
         alt = params .name;
 
-    return `
-        <picture>
-            <img src="${ src }" alt="${ alt }" />
-            <p>${ params .name }</p>
-        </picture>
-    `;
+    return el( 'img', {
+        src, alt
+    });
 }
 
-$app .innerHTML += Avatar({ id: 75, name: 'Ana María' });
-$app .innerHTML += Avatar({ id: 77, name: 'Luisa María' });
-$app .innerHTML += Avatar({ id: 79, name: 'Elisa María' });
+ReactDOM .render( el( Avatar, { id: 75, name: 'Ana María' } ), $app );
 
-$app .querySelectorAll( 'img') .forEach( img => {
-    img .addEventListener( 'click', () => {
-        img .classList .toggle( 'disabled' );
-    });
-});
