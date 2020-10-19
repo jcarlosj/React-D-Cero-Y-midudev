555bbdd46de07ba5fc88dc7fbbf7db1bfc09ebe7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-18 20:11:07 -0500

    [+] Avatar: Crea Componente usando JavaScript

---
+++ js/avatar.js
@@ -0,0 +1,18 @@
+const $app = document .getElementById( 'app' );
+
+const Avatar = params => {
+    const 
+        src = `https://randomuser.me/api/portraits/women/${ params .id }.jpg`,
+        alt = params .name;
+
+    return `
+        <picture>
+            <img src="${ src }" alt="${ alt }" />
+            <p>${ params .name }</p>
+        </picture>
+    `;
+}
+
+$app .innerHTML += Avatar({ id: 75, name: 'Ana María' });
+$app .innerHTML += Avatar({ id: 77, name: 'Luisa María' });
+$app .innerHTML += Avatar({ id: 79, name: 'Elisa María' });
