c939b589a30f656c3d3aa9df3993631db312c3c5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-20 15:43:15 -0500

    [+] Avatar: Renderiza condicionalmente el componente solo si posee id

--- js/avatar.js
+++ js/avatar.js
@@ -1,19 +1,19 @@
 
 const 
     $app = document .getElementById( 'app' ),
-    useState = React .useState;
+    { useState, Fragment } = React;
 
-const Avatar = props => {
+const Avatar = ({ id, name='Desconocido/a', size }) => {
     const 
         [ enabled, setEnabled ] = useState( true ),
-        src = `https://randomuser.me/api/portraits/women/${ props .id }.jpg`,
-        alt = props .name,
+        src = `https://randomuser.me/api/portraits/women/${ id }.jpg`,
+        alt = name,
         imgClassName = enabled ? '' : 'disabled';
 
     const getSize = () => {
-        if( props .size === 'small' ) {
+        if( size === 'small' ) {
             return 'is-small';
-        } else if( props .size === 'large' ) {
+        } else if( size === 'large' ) {
             return 'is-large';
         } else {
             return '';
@@ -21,22 +21,28 @@ const Avatar = props => {
     }
 
     return (
-        <picture className={ getSize() }>
-            <img 
-                className={ `${ getSize() } ${ imgClassName }` }
-                src={ src } 
-                alt={ alt } 
-                onClick={ () => setEnabled( ! enabled ) }
-            />
-            <p>{ alt }</p>
-        </picture>
+        <Fragment>
+            { id 
+                ?   <picture className={ getSize() }>
+                        <img 
+                            className={ `${ getSize() } ${ imgClassName }` }
+                            src={ src } 
+                            alt={ alt } 
+                            onClick={ () => setEnabled( ! enabled ) }
+                        />
+                        <p>{ alt }</p>
+                    </picture>
+                :   null
+            }
+        </Fragment>
     );
 }
 
 ReactDOM .render( 
     <div>
+        <Avatar />
         <Avatar id={ 75 } name={ 'Ana María' } size="small" />
-        <Avatar id={ 77 } name={ 'Luisa María' } />
+        <Avatar id={ 77 } />
         <Avatar id={ 79 } name={ 'Elisa María' } size="large" />
     </div>,
     $app
