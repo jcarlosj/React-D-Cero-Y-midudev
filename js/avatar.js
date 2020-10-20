4735a206681acd261cf8b3060be31cc14138bbf4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-20 15:30:18 -0500

    [+] Avatar: Crea tamano large a partir del tamano normal

--- js/avatar.js
+++ js/avatar.js
@@ -8,13 +8,22 @@ const Avatar = props => {
         [ enabled, setEnabled ] = useState( true ),
         src = `https://randomuser.me/api/portraits/women/${ props .id }.jpg`,
         alt = props .name,
-        smallClassName = props .size === 'small' ? 'is-small' : '',
         imgClassName = enabled ? '' : 'disabled';
 
+    const getSize = () => {
+        if( props .size === 'small' ) {
+            return 'is-small';
+        } else if( props .size === 'large' ) {
+            return 'is-large';
+        } else {
+            return '';
+        }
+    }
+
     return (
-        <picture className={ smallClassName }>
+        <picture className={ getSize() }>
             <img 
-                className={ `${ smallClassName } ${ imgClassName }` }
+                className={ `${ getSize() } ${ imgClassName }` }
                 src={ src } 
                 alt={ alt } 
                 onClick={ () => setEnabled( ! enabled ) }
@@ -28,7 +37,7 @@ ReactDOM .render(
     <div>
         <Avatar id={ 75 } name={ 'Ana María' } size="small" />
         <Avatar id={ 77 } name={ 'Luisa María' } />
-        <Avatar id={ 79 } name={ 'Elisa María' } />
+        <Avatar id={ 79 } name={ 'Elisa María' } size="large" />
     </div>,
     $app
 );
